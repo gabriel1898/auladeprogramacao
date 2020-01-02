54907640b55987c7c81773a67b39d030e50ffb1e
gabriel1898 <gabrielgsobral@gmail.com>
2020-01-02 02:42:14 +0000

    atualizei o main

--- estruturaSequencial/main.py
+++ estruturaSequencial/main.py
@@ -2,10 +2,13 @@ import areaQuadrado07
 import alomundo01
 import soma03
 import baskara
+import celsius09
 #alomundo01.aloMundo()
 
 #soma03.soma()
 
 #areaQuadrado07.ladoquadrado()
 
-baskara.baskara()
+#baskara.baskara()
+
+celsius09.celsius()
