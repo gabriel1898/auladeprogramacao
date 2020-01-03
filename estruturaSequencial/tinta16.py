08c6653db49f2eb8528a09da5feb4a5eb07ef9eb
gabriel1898 <gabrielgsobral@gmail.com>
2020-01-03 21:25:57 +0000

    estrutura condicional exercicio 16

--- estruturaSequencial/tinta16.py
+++ estruturaSequencial/tinta16.py
@@ -2,7 +2,12 @@ def custo():
   area = input ("valor area")
   litros = float(area)/3
   print (litros)
-  latas = float(litros)/18
-  print (latas)
-  custo = latas*80
+  latasFloat = float(litros)/18
+  latasInt = int(latasFloat)
+  if (latasFloat > latasInt): 
+    latasInt = latasInt + 1
+  print (latasFloat)
+  print (latasInt)
+  custo = latasInt*80
   print (custo)
+
