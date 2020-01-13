cb5195520c2f26587ada8eac80a91c0a26925809
fegvilela <fegvilela@gmail.com>
2020-01-13 19:28:15 -0200

    add gabarito exercicio02 estruturaRepeticao

--- gabarito/estruturaRepeticao/exercicio02.py
+++ gabarito/estruturaRepeticao/exercicio02.py
@@ -6,17 +6,34 @@
 def splitString(word):
     return [x for x in word]
 
+def checkEqual(word1, word2):
+    #loop para mover de caractere em caractere na palavra
+    for x in word1:
+        if x != word2[word1.index(x)]:
+            print("operação bem sucedida")
+            igual = False
+            break
+    else:
+        igual = True
+        print("o login é igual à senha, mude um dos dois")
+    return igual
 
 
 
-#pede login e senha do usuário
-login = input("login: ")
-password = input("password: ")
+#variável para avaliar se deve voltar a pedir a senha e o login (caso sejam iguais)
+igual = True
 
-loginChar = splitString(login)
-passwordChar = splitString(password)
 
-print(loginChar, passwordChar)
+while (igual == True):
+    #pede login e senha do usuário
+    login = input("login: ")
+    password = input("password: ")
+
+    #transforma string em lista
+    login = splitString(login)
+    password = splitString(password)
+
+    igual = checkEqual(login, password)
 
 
 
