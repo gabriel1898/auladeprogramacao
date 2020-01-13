6f0531a7fa8d0af276e9251e659a14dec8f9e0fd
fegvilela <fegvilela@gmail.com>
2020-01-13 21:33:26 -0200

    add gabarito exercicio03 estrutura repeticao

--- gabarito/estruturaRepeticao/exercicio03.py
+++ gabarito/estruturaRepeticao/exercicio03.py
@@ -6,19 +6,80 @@
 # Estado Civil: 's', 'c', 'v', 'd';
 
 
-right = False
 
-while (right == False):
-    nota = input("coloque o valor de 0 a 10: ")
-    if int(nota) > 10 or int(nota) < 0:
-        print("valor invalido")
-        certo = False
+def validarNome(nome):
+    if len(nome) >= 3:
+        return True
     else:
-        certo = True
+        return False
+
+
+def validarIdade(idade):
+    if idade >= 0 and idade <= 150:
+        return True
+    else:
+        return False
+
+def validarSalario(salario):
+    if salario > 0:
+        return True
+    else:
+        return False
+
+def validarSexo(sexo):
+    if sexo == 'f' or sexo == 'm':
+        return True
+    else:
+        return False
+
+def validarEstadoCivil(estadoCivil):
+    if estadoCivil == 's' or estadoCivil == 'c' or estadoCivil == 'v' or estadoCivil == 'd':
+        return True
+    else:
+        return False
+
+
+def main():
+
+    validacaoNome = False
+    validacaoIdade = False
+    validacaoSalario = False
+    validacaoSexo = False
+    validacaoEstadoCivil = False
+    while(validacaoNome == False):
+        nome = input("coloque seu nome: ")
+        if validarNome(nome) == True:
+            break
+        print("nome invalido, insira novamente")
+
+
+    while(validacaoNome == False):
+        idade = input("coloque sua idade: ")
+        if validarIdade(int(idade)) == True:
+            break
+        print("idade invalido, insira novamente")
+
+
+    while(validacaoSalario == False):
+        salario = input("coloque seu salario: ")
+        if validarSalario(int(salario)) == True:
+            break
+        print("salario invalido, insira novamente")
+
+    while(validacaoSexo == False):
+        sexo = input("coloque seu sexo [f ou m]: ")
+        if validarSexo(sexo) == True:
+            break
+        print("sexo invalido, insira novamente")
+
+    while(validacaoEstadoCivil == False):
+        estadoCivil = input("coloque seu estado civil [s, c, d ou v]: ")
+        if validarEstadoCivil(estadoCivil) == True:
+            break
+        print("estado civil invalido, insira novamente")
 
 
 
-def validacaoNome(nome):
-    contador = 0
-    for x in nome:
-        count = count + 1
+main()
+
+
