b8e2968b62d676a9eecb8fb5db797d87d772a48d
fegvilela <fegvilela@gmail.com>
2020-01-14 00:42:40 -0200

    finish gabarito exercicio07 estrutura repeticao

--- gabarito/estruturaRepeticao/exercicio07.py
+++ gabarito/estruturaRepeticao/exercicio07.py
@@ -1,3 +1,32 @@
 # Faça um programa que leia 5 números e informe o maior número.
 
 
+
+
+def substituirReferencia(num1,referenciaMaior):
+    if num1 > referenciaMaior:
+        referenciaMaior = num1
+    return referenciaMaior
+
+
+def main():
+
+    referenciaMaior = 0
+
+    numList=[]
+
+    for x in range(1,6):
+        numInput = float(input("informe o %d número: " % x))
+        numList.append(numInput)
+
+    for x in numList:
+        referenciaMaior = substituirReferencia(x, referenciaMaior)
+
+    print(referenciaMaior)
+
+
+
+
+# o main só será chamado quando exercutar esse arquivo
+if __name__ == "__main__":
+   main()
