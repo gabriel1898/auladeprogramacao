af2058f72324ea090ffe334518677fd349ebad6d
gabriel sobral <gabrielgsobral@gmail.com>
2020-01-15 19:16:22 -0300

    exercicio2

--- estruturaRepeticao/exercicio02.py
+++ estruturaRepeticao/exercicio02.py
@@ -1,7 +1,34 @@
+#def seIgual(usuario, password):
+#	if usuario == password:
+#		return True
+#	else:
+#		return False
+
 def exercicio2():
-  usuario = ["augusto"]
-  password = ["augusto"]
-  for x in usuario:
-    for y in password:
-      if x == y:
-        print ("error")
+	usuario =input("usuario")
+	password = input("password")
+	while usuario == password:
+		print ("error")
+		usuario =input("usuario")
+		password = input("password")
+
+#	for x in usuario:
+#		print("x", x)
+#		for y in password:
+#			print("y", y)
+#			if x == y:
+#				print ("error", x)
+
+def main():
+	exercicio2()
+
+#	usuario =input("usuario")
+#	password = input("password")
+#	while seIgual(usuario, password):
+#		print ("error")
+#		usuario =input("usuario")
+#		password = input("password")
+
+
+if __name__ == "__main__":
+	main()
--- testes/guardaroupa.py
+++ testes/guardaroupa.py
@@ -1,6 +1,9 @@
-bermudas = []
-camisas = []
+bermudas = ["amarelo", "azul", "branco"]
+camisas = ["preto", "vermelho"]
 for x in bermudas:
+	for y in camisas:
 		print ("a bermuda escolhida foi x")
 		print (x
-			 )
+		 )
+		print("a camisa é")
+		print(y)
