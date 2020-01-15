cbd5f7da05246bfe346786960dee4fb14fcf7cb6
gabriel sobral <gabrielgsobral@gmail.com>
2020-01-15 18:20:45 -0300

    atualizacao do exemplo while

--- testes/exemploWhile.py
+++ testes/exemploWhile.py
@@ -4,9 +4,32 @@ def imprimirLoop(vezes):
 		print (i)
 		i += 1
 
+def banana(vezes, onOff):
+	if onOff == True:
+		i = 0
+		while i <= vezes:
+			print (i)
+			if i == (vezes - 2):
+				break
+			i += 1
+	if onOff == False:
+		imprimirLoop(vezes)
+
 def main():
-	vezes = int(input("quantas vezes deseja imprimir? "))
-	imprimirLoop(vezes)
+
+	vezes = int(input("quantas vezes deseja imprimir?"))
+	onOff = input ("is onOff true or false?   T for True   or   F for False")
+	while onOff != "T" and onOff != "F": 
+		onOff = input ("is onOff true or false?   T for True   or   F for False")
+	if onOff =="T":
+		onOff = True
+	if onOff =="F":
+		onOff = False
+	banana(vezes, onOff)
+	print(type(onOff))
+
+#	vezes = int(input("quantas vezes deseja imprimir? "))
+#	imprimirLoop(vezes)
 
 if __name__ == "__main__":
 	main()
