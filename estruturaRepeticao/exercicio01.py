bbc375507452ed4fc1acf137dec242287d2dd2bb
gabriel sobral <gabrielgsobral@gmail.com>
2020-01-15 18:40:51 -0300

    exercicio1 estrutura repeticao

--- estruturaRepeticao/exercicio01.py
+++ estruturaRepeticao/exercicio01.py
@@ -1,7 +1,17 @@
 def exercicio1():
-  numero = 3
-  for x in range(10):
-    if x == numero:
-      print (numero)
+	p = float(input("qual é a nota?"))
+	while p >= 10 or p <= 0:
+		print ("invalid")
+		p = float(input("qual é a nota?"))
+	print ("valor correto")
+
+
+def main():
+	exercicio1()
+
+
+if __name__ == "__main__":
+	main()
+
 
 
